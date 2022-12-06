71785d82e658a2ecc7c9c6cb696a4802b91d38d1
iamzjt-front-end <iamzjt@foxmail.com>
2022-12-06 20:31:27 +0800

    :zap: feat: reactive.ts shared 注释补充

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,34 +1,27 @@
-import { TrackOpTypes, TriggerOpTypes } from './operations'
-import { extend, isArray, isIntegerKey, isMap, toNumber } from '@vue/shared'
-import { EffectScope, recordEffectScope } from './effectScope'
-import {
-  createDep,
-  Dep,
-  finalizeDepMarkers,
-  initDepMarkers,
-  newTracked,
-  wasTracked
-} from './dep'
-import { ComputedRefImpl } from './computed'
+import { TrackOpTypes, TriggerOpTypes } from './operations';
+import { extend, isArray, isIntegerKey, isMap, toNumber } from '@vue/shared';
+import { EffectScope, recordEffectScope } from './effectScope';
+import { createDep, Dep, finalizeDepMarkers, initDepMarkers, newTracked, wasTracked } from './dep';
+import { ComputedRefImpl } from './computed';
 
 // The main WeakMap that stores {target -> key -> dep} connections.
 // Conceptually, it's easier to think of a dependency as a Dep class
 // which maintains a Set of subscribers, but we simply store them as
 // raw Sets to reduce memory overhead.
 type KeyToDepMap = Map<any, Dep>
-const targetMap = new WeakMap<any, KeyToDepMap>()
+const targetMap = new WeakMap<any, KeyToDepMap>();
 
 // The number of effects currently being tracked recursively.
-let effectTrackDepth = 0
+let effectTrackDepth = 0;
 
-export let trackOpBit = 1
+export let trackOpBit = 1;
 
 /**
  * The bitwise track markers support at most 30 levels of recursion.
  * This value is chosen to enable modern JS engines to use a SMI on all platforms.
  * When recursion depth is greater, fall back to using a full cleanup.
  */
-const maxMarkerBits = 30
+const maxMarkerBits = 30;
 
 export type EffectScheduler = (...args: any[]) => any
 
@@ -45,82 +38,83 @@ export type DebuggerEventExtraInfo = {
   oldTarget?: Map<any, any> | Set<any>
 }
 
-export let activeEffect: ReactiveEffect | undefined
+// + 当前激活的effect
+export let activeEffect: ReactiveEffect | undefined;
 
-export const ITERATE_KEY = Symbol(__DEV__ ? 'iterate' : '')
-export const MAP_KEY_ITERATE_KEY = Symbol(__DEV__ ? 'Map key iterate' : '')
+export const ITERATE_KEY = Symbol(__DEV__ ? 'iterate' : '');
+export const MAP_KEY_ITERATE_KEY = Symbol(__DEV__ ? 'Map key iterate' : '');
 
 export class ReactiveEffect<T = any> {
-  active = true
-  deps: Dep[] = []
-  parent: ReactiveEffect | undefined = undefined
+  active = true;
+  deps: Dep[] = [];
+  parent: ReactiveEffect | undefined = undefined;
 
   /**
    * Can be attached after creation
    * @internal
    */
-  computed?: ComputedRefImpl<T>
+  computed?: ComputedRefImpl<T>;
   /**
    * @internal
    */
-  allowRecurse?: boolean
+  allowRecurse?: boolean;
   /**
    * @internal
    */
-  private deferStop?: boolean
+  private deferStop?: boolean;
 
-  onStop?: () => void
+  onStop?: () => void;
   // dev only
-  onTrack?: (event: DebuggerEvent) => void
+  onTrack?: (event: DebuggerEvent) => void;
   // dev only
-  onTrigger?: (event: DebuggerEvent) => void
+  onTrigger?: (event: DebuggerEvent) => void;
 
   constructor(
     public fn: () => T,
     public scheduler: EffectScheduler | null = null,
     scope?: EffectScope
   ) {
-    recordEffectScope(this, scope)
+    recordEffectScope(this, scope);
   }
 
   run() {
     if (!this.active) {
-      return this.fn()
+      return this.fn();
     }
-    let parent: ReactiveEffect | undefined = activeEffect
-    let lastShouldTrack = shouldTrack
+    let parent: ReactiveEffect | undefined = activeEffect;
+    let lastShouldTrack = shouldTrack;
     while (parent) {
       if (parent === this) {
-        return
+        return;
       }
-      parent = parent.parent
+      parent = parent.parent;
     }
     try {
-      this.parent = activeEffect
-      activeEffect = this
-      shouldTrack = true
+      this.parent = activeEffect;
+      activeEffect = this;
+      shouldTrack = true;
 
-      trackOpBit = 1 << ++effectTrackDepth
+      trackOpBit = 1 << ++effectTrackDepth;
 
       if (effectTrackDepth <= maxMarkerBits) {
-        initDepMarkers(this)
+        initDepMarkers(this);
       } else {
-        cleanupEffect(this)
+        cleanupEffect(this);
       }
-      return this.fn()
+      return this.fn();
     } finally {
       if (effectTrackDepth <= maxMarkerBits) {
-        finalizeDepMarkers(this)
+        finalizeDepMarkers(this);
       }
 
-      trackOpBit = 1 << --effectTrackDepth
+      trackOpBit = 1 << --effectTrackDepth;
 
-      activeEffect = this.parent
-      shouldTrack = lastShouldTrack
-      this.parent = undefined
+      activeEffect = this.parent;
+      shouldTrack = lastShouldTrack;
+      this.parent = undefined;
 
       if (this.deferStop) {
-        this.stop()
+        this.stop();
       }
     }
   }
@@ -128,104 +122,106 @@ export class ReactiveEffect<T = any> {
   stop() {
     // stopped while running itself - defer the cleanup
     if (activeEffect === this) {
-      this.deferStop = true
+      this.deferStop = true;
     } else if (this.active) {
-      cleanupEffect(this)
+      cleanupEffect(this);
       if (this.onStop) {
-        this.onStop()
+        this.onStop();
       }
-      this.active = false
+      this.active = false;
     }
   }
 }
 
 function cleanupEffect(effect: ReactiveEffect) {
-  const { deps } = effect
+  const { deps } = effect;
   if (deps.length) {
     for (let i = 0; i < deps.length; i++) {
-      deps[i].delete(effect)
+      deps[i].delete(effect);
     }
-    deps.length = 0
+    deps.length = 0;
   }
 }
 
 export interface DebuggerOptions {
-  onTrack?: (event: DebuggerEvent) => void
-  onTrigger?: (event: DebuggerEvent) => void
+  onTrack?: (event: DebuggerEvent) => void;
+  onTrigger?: (event: DebuggerEvent) => void;
 }
 
 export interface ReactiveEffectOptions extends DebuggerOptions {
-  lazy?: boolean
-  scheduler?: EffectScheduler
-  scope?: EffectScope
-  allowRecurse?: boolean
-  onStop?: () => void
+  lazy?: boolean;
+  scheduler?: EffectScheduler;
+  scope?: EffectScope;
+  allowRecurse?: boolean;
+  onStop?: () => void;
 }
 
 export interface ReactiveEffectRunner<T = any> {
-  (): T
-  effect: ReactiveEffect
+  (): T;
+
+  effect: ReactiveEffect;
 }
 
 export function effect<T = any>(
   fn: () => T,
   options?: ReactiveEffectOptions
 ): ReactiveEffectRunner {
+  // +
   if ((fn as ReactiveEffectRunner).effect) {
-    fn = (fn as ReactiveEffectRunner).effect.fn
+    fn = (fn as ReactiveEffectRunner).effect.fn;
   }
 
-  const _effect = new ReactiveEffect(fn)
+  const _effect = new ReactiveEffect(fn);
   if (options) {
-    extend(_effect, options)
-    if (options.scope) recordEffectScope(_effect, options.scope)
+    extend(_effect, options);
+    if (options.scope) recordEffectScope(_effect, options.scope);
   }
   if (!options || !options.lazy) {
-    _effect.run()
+    _effect.run();
   }
-  const runner = _effect.run.bind(_effect) as ReactiveEffectRunner
-  runner.effect = _effect
-  return runner
+  const runner = _effect.run.bind(_effect) as ReactiveEffectRunner;
+  runner.effect = _effect;
+  return runner;
 }
 
 export function stop(runner: ReactiveEffectRunner) {
-  runner.effect.stop()
+  runner.effect.stop();
 }
 
-export let shouldTrack = true
-const trackStack: boolean[] = []
+export let shouldTrack = true;
+const trackStack: boolean[] = [];
 
 export function pauseTracking() {
-  trackStack.push(shouldTrack)
-  shouldTrack = false
+  trackStack.push(shouldTrack);
+  shouldTrack = false;
 }
 
 export function enableTracking() {
-  trackStack.push(shouldTrack)
-  shouldTrack = true
+  trackStack.push(shouldTrack);
+  shouldTrack = true;
 }
 
 export function resetTracking() {
-  const last = trackStack.pop()
-  shouldTrack = last === undefined ? true : last
+  const last = trackStack.pop();
+  shouldTrack = last === undefined ? true : last;
 }
 
 export function track(target: object, type: TrackOpTypes, key: unknown) {
   if (shouldTrack && activeEffect) {
-    let depsMap = targetMap.get(target)
+    let depsMap = targetMap.get(target);
     if (!depsMap) {
-      targetMap.set(target, (depsMap = new Map()))
+      targetMap.set(target, (depsMap = new Map()));
     }
-    let dep = depsMap.get(key)
+    let dep = depsMap.get(key);
     if (!dep) {
-      depsMap.set(key, (dep = createDep()))
+      depsMap.set(key, (dep = createDep()));
     }
 
     const eventInfo = __DEV__
       ? { effect: activeEffect, target, type, key }
-      : undefined
+      : undefined;
 
-    trackEffects(dep, eventInfo)
+    trackEffects(dep, eventInfo);
   }
 }
 
@@ -233,25 +229,25 @@ export function trackEffects(
   dep: Dep,
   debuggerEventExtraInfo?: DebuggerEventExtraInfo
 ) {
-  let shouldTrack = false
+  let shouldTrack = false;
   if (effectTrackDepth <= maxMarkerBits) {
     if (!newTracked(dep)) {
-      dep.n |= trackOpBit // set newly tracked
-      shouldTrack = !wasTracked(dep)
+      dep.n |= trackOpBit; // set newly tracked
+      shouldTrack = !wasTracked(dep);
     }
   } else {
     // Full cleanup mode.
-    shouldTrack = !dep.has(activeEffect!)
+    shouldTrack = !dep.has(activeEffect!);
   }
 
   if (shouldTrack) {
-    dep.add(activeEffect!)
-    activeEffect!.deps.push(dep)
+    dep.add(activeEffect!);
+    activeEffect!.deps.push(dep);
     if (__DEV__ && activeEffect!.onTrack) {
       activeEffect!.onTrack({
         effect: activeEffect!,
         ...debuggerEventExtraInfo!
-      })
+      });
     }
   }
 }
@@ -264,82 +260,82 @@ export function trigger(
   oldValue?: unknown,
   oldTarget?: Map<unknown, unknown> | Set<unknown>
 ) {
-  const depsMap = targetMap.get(target)
+  const depsMap = targetMap.get(target);
   if (!depsMap) {
     // never been tracked
-    return
+    return;
   }
 
-  let deps: (Dep | undefined)[] = []
+  let deps: (Dep | undefined)[] = [];
   if (type === TriggerOpTypes.CLEAR) {
     // collection being cleared
     // trigger all effects for target
-    deps = [...depsMap.values()]
+    deps = [...depsMap.values()];
   } else if (key === 'length' && isArray(target)) {
-    const newLength = toNumber(newValue)
+    const newLength = toNumber(newValue);
     depsMap.forEach((dep, key) => {
       if (key === 'length' || key >= newLength) {
-        deps.push(dep)
+        deps.push(dep);
       }
-    })
+    });
   } else {
     // schedule runs for SET | ADD | DELETE
     if (key !== void 0) {
-      deps.push(depsMap.get(key))
+      deps.push(depsMap.get(key));
     }
 
     // also run for iteration key on ADD | DELETE | Map.SET
     switch (type) {
       case TriggerOpTypes.ADD:
         if (!isArray(target)) {
-          deps.push(depsMap.get(ITERATE_KEY))
+          deps.push(depsMap.get(ITERATE_KEY));
           if (isMap(target)) {
-            deps.push(depsMap.get(MAP_KEY_ITERATE_KEY))
+            deps.push(depsMap.get(MAP_KEY_ITERATE_KEY));
           }
         } else if (isIntegerKey(key)) {
           // new index added to array -> length changes
-          deps.push(depsMap.get('length'))
+          deps.push(depsMap.get('length'));
         }
-        break
+        break;
       case TriggerOpTypes.DELETE:
         if (!isArray(target)) {
-          deps.push(depsMap.get(ITERATE_KEY))
+          deps.push(depsMap.get(ITERATE_KEY));
           if (isMap(target)) {
-            deps.push(depsMap.get(MAP_KEY_ITERATE_KEY))
+            deps.push(depsMap.get(MAP_KEY_ITERATE_KEY));
           }
         }
-        break
+        break;
       case TriggerOpTypes.SET:
         if (isMap(target)) {
-          deps.push(depsMap.get(ITERATE_KEY))
+          deps.push(depsMap.get(ITERATE_KEY));
         }
-        break
+        break;
     }
   }
 
   const eventInfo = __DEV__
     ? { target, type, key, newValue, oldValue, oldTarget }
-    : undefined
+    : undefined;
 
   if (deps.length === 1) {
     if (deps[0]) {
       if (__DEV__) {
-        triggerEffects(deps[0], eventInfo)
+        triggerEffects(deps[0], eventInfo);
       } else {
-        triggerEffects(deps[0])
+        triggerEffects(deps[0]);
       }
     }
   } else {
-    const effects: ReactiveEffect[] = []
+    const effects: ReactiveEffect[] = [];
     for (const dep of deps) {
       if (dep) {
-        effects.push(...dep)
+        effects.push(...dep);
       }
     }
     if (__DEV__) {
-      triggerEffects(createDep(effects), eventInfo)
+      triggerEffects(createDep(effects), eventInfo);
     } else {
-      triggerEffects(createDep(effects))
+      triggerEffects(createDep(effects));
     }
   }
 }
@@ -349,15 +345,15 @@ export function triggerEffects(
   debuggerEventExtraInfo?: DebuggerEventExtraInfo
 ) {
   // spread into array for stabilization
-  const effects = isArray(dep) ? dep : [...dep]
+  const effects = isArray(dep) ? dep : [...dep];
   for (const effect of effects) {
     if (effect.computed) {
-      triggerEffect(effect, debuggerEventExtraInfo)
+      triggerEffect(effect, debuggerEventExtraInfo);
     }
   }
   for (const effect of effects) {
     if (!effect.computed) {
-      triggerEffect(effect, debuggerEventExtraInfo)
+      triggerEffect(effect, debuggerEventExtraInfo);
     }
   }
 }
@@ -368,12 +364,12 @@ function triggerEffect(
 ) {
   if (effect !== activeEffect || effect.allowRecurse) {
     if (__DEV__ && effect.onTrigger) {
-      effect.onTrigger(extend({ effect }, debuggerEventExtraInfo))
+      effect.onTrigger(extend({ effect }, debuggerEventExtraInfo));
     }
     if (effect.scheduler) {
-      effect.scheduler()
+      effect.scheduler();
     } else {
-      effect.run()
+      effect.run();
     }
   }
 }
